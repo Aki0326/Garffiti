8fe2cc4f471b54fd89e5513cdd41ebe838f2bdf2
Aki0326 <s1671108@gmail.com>
2019-06-21 15:27:46 +0900

    #5 写真ギャラリーを自作、タイムアウト機能を追加しました。

--- app/src/main/java/com/google/ar/core/examples/java/helloar/ModeSelectActivity.java
+++ app/src/main/java/com/google/ar/core/examples/java/helloar/ModeSelectActivity.java
@@ -2,16 +2,19 @@ package com.google.ar.core.examples.java.helloar;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
 public class ModeSelectActivity extends AppCompatActivity implements View.OnClickListener {
-    private static final String TAG = ModeSelectActivity.class.getSimpleName();
+    public static final String TAG = ModeSelectActivity.class.getSimpleName();
 
     private Button graffityModeButton;
     private Button coloringbattleModeButton;
     private Button photogalleryButton;
+
+    private PowerManager.WakeLock wakeLock;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,25 @@ public class ModeSelectActivity extends AppCompatActivity implements View.OnClic
 
         photogalleryButton = findViewById(R.id.photogallery_button);
         photogalleryButton.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(wakeLock!=null){
+            wakeLock.acquire();
+        }else{
+            PowerManager pm = (PowerManager)getSystemService(POWER_SERVICE);
+            wakeLock = pm.newWakeLock(PowerManager.SCREEN_DIM_WAKE_LOCK, ":tag");
+            wakeLock.acquire();
+        }
+}
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        wakeLock.release();
     }
 
     @Override
