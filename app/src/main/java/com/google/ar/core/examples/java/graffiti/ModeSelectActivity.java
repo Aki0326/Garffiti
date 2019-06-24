dbc70935c47bf0d7e697f311319d1597d158a174
Aki Hongo <s1671108@s.konan-u.ac.jp>
2019-06-24 14:25:17 +0900

    モードタップ時、サイズが大きくなるよう変更

--- app/src/main/java/com/google/ar/core/examples/java/graffiti/ModeSelectActivity.java
+++ app/src/main/java/com/google/ar/core/examples/java/graffiti/ModeSelectActivity.java
@@ -4,15 +4,20 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.PowerManager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.google.ar.core.examples.java.common.helpers.MusicPlayerHelper;
 
 import java.io.IOException;
 
-public class ModeSelectActivity extends AppCompatActivity implements View.OnClickListener {
+public class ModeSelectActivity extends AppCompatActivity implements View.OnClickListener, View.OnTouchListener {
     public static final String TAG = ModeSelectActivity.class.getSimpleName();
 
     private MusicPlayerHelper modeSelectBGM = new MusicPlayerHelper();
@@ -33,12 +38,15 @@ public class ModeSelectActivity extends AppCompatActivity implements View.OnClic
 
         graffityModeButton = findViewById(R.id.graffity_mode_button);
         graffityModeButton.setOnClickListener(this);
+        graffityModeButton.setOnTouchListener(this);
 
         coloringbattleModeButton = findViewById(R.id.coloringbattle_mode_button);
         coloringbattleModeButton.setOnClickListener(this);
+        coloringbattleModeButton.setOnTouchListener(this);
 
         photogalleryButton = findViewById(R.id.photogallery_button);
         photogalleryButton.setOnClickListener(this);
+        photogalleryButton.setOnTouchListener(this);
     }
 
     @Override
@@ -79,20 +87,29 @@ public class ModeSelectActivity extends AppCompatActivity implements View.OnClic
 
     @Override
     public void onClick(View view) {
-        modeSelectBGM.musicStop();
-        try {
-            isLoop = false;
-            modeSelectClickSE.musicPlay(this, "musics/se/click-sound.mp3", isLoop);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        modeSelectClickSE.musicStop();
         switch (view.getId()) {
             case R.id.graffity_mode_button:
+                modeSelectBGM.musicStop();
+                try {
+                    isLoop = false;
+                    modeSelectClickSE.musicPlay(this, "musics/se/click-sound.mp3", isLoop);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 startActivity(new Intent(ModeSelectActivity.this, HelloArActivity.class));
                 break;
             case R.id.coloringbattle_mode_button:
+                Toast.makeText(this, "すみません。ただいま工事中です。", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.photogallery_button:
+                modeSelectBGM.musicStop();
+                try {
+                    isLoop = false;
+                    modeSelectClickSE.musicPlay(this, "musics/se/click-sound.mp3", isLoop);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 startActivity(new Intent(ModeSelectActivity.this, PhotoGalleryActivity.class));
                 break;
         }
@@ -107,5 +124,32 @@ public class ModeSelectActivity extends AppCompatActivity implements View.OnClic
             }
         }
         return super.dispatchKeyEvent(event);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (view.getId()) {
+            case R.id.graffity_mode_button:
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    graffityModeButton.setScaleX(1.2f);
+                    graffityModeButton.setScaleY(1.2f);
+                } else if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    graffityModeButton.setScaleX(1.0f);
+                    graffityModeButton.setScaleY(1.0f);
+                }
+                break;
+            case R.id.coloringbattle_mode_button:
+                break;
+            case R.id.photogallery_button:
+                if(motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    photogalleryButton.setScaleX(1.2f);
+                    photogalleryButton.setScaleY(1.2f);
+                } else if(motionEvent.getAction() == MotionEvent.ACTION_UP) {
+                    photogalleryButton.setScaleX(1.0f);
+                    photogalleryButton.setScaleY(1.0f);
+                }
+                break;
+        }
+        return false;
     }
 }
