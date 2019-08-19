646441a4cc2c6eab8e35ba8ea36d0f7ba6881591
Aki Hongo <s1671108@s.konan-u.ac.jp>
2019-08-19 15:55:45 +0900

    全体のパッケージ名を変更

---
+++ app/src/main/java/org/ntlab/graffiti/common/drawer/TextureDrawer.java
@@ -0,0 +1,13 @@
+package org.ntlab.graffiti.common.drawer;
+
+import android.graphics.Canvas;
+
+abstract public class TextureDrawer {
+    protected int color;
+
+    public TextureDrawer(int color) {
+        this.color = color;
+    }
+
+    abstract public void draw(int pixelX, int pixelY, int r, Canvas canvas);
+}
