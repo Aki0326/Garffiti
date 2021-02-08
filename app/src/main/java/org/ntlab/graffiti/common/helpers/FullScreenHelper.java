9fc86592a1be0478fd5e0879930f08fa1245372f
Aki Hongo <s1671108@s.konan-u.ac.jp>
2021-02-08 18:47:05 +0900

    Update helpers class.

--- app/src/main/java/org/ntlab/graffiti/common/helpers/FullScreenHelper.java
+++ app/src/main/java/org/ntlab/graffiti/common/helpers/FullScreenHelper.java
@@ -1,5 +1,6 @@
 /*
- * Copyright 2017 Google Inc. All Rights Reserved.
+ * Copyright 2017 Google LLC
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -16,7 +17,6 @@ package org.ntlab.graffiti.common.helpers;
 
 import android.app.Activity;
 import android.view.View;
-import android.view.WindowManager;
 
 /** Helper to set up the Android full screen mode. */
 public final class FullScreenHelper {
@@ -32,16 +32,15 @@ public final class FullScreenHelper {
     if (hasFocus) {
       // https://developer.android.com/training/system-ui/immersive.html#sticky
       activity
-          .getWindow()
-          .getDecorView()
-          .setSystemUiVisibility(
-              View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                  | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                  | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                  | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                  | View.SYSTEM_UI_FLAG_FULLSCREEN
-                  | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
-      activity.getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+              .getWindow()
+              .getDecorView()
+              .setSystemUiVisibility(
+                      View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                              | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                              | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                              | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                              | View.SYSTEM_UI_FLAG_FULLSCREEN
+                              | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
     }
   }
 }
